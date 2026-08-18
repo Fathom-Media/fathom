e6c7e2aa8d20bc2a8291e5d6dfd6f4920c263334
Sébastien Bariteau <numkem@numke.org>
2026-08-18 11:39:05 -0400

    add Nix flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "A modern desktop client for Jellyfin, with an optional built-in YouTube player and Seerr requests.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = inputs: {
+    packages = builtins.mapAttrs (system: pkgs: rec {
+      default = fathom;
+
+      fathom = pkgs.flutter344.buildFlutterApplication (finalAttrs: {
+        pname = "fathom";
+        version = "0.11.1";
+
+        src = ./.;
+
+        buildInputs = with pkgs; [ mpv ];
+
+        autoPubspecLock = ./pubspec.lock;
+
+        postInstall = ''
+          install -Dm644 ${finalAttrs.src}/linux/packaging/icons/fathom-512.png $out/share/icons/hicolor/512x512/apps/app.fathom.player.png
+          install -Dm644 ${finalAttrs.src}/linux/packaging/icons/fathom-256.png $out/share/icons/hicolor/256x256/apps/app.fathom.player.png
+          install -Dm644 ${finalAttrs.src}/linux/packaging/icons/fathom-128.png $out/share/icons/hicolor/128x128/apps/app.fathom.player.png
+          install -Dm644 ${finalAttrs.src}/linux/packaging/app.fathom.player.desktop $out/share/applications/fathom.desktop
+        '';
+      });
+    }) inputs.nixpkgs.legacyPackages;
+
+    overlays.default = final: prev: {
+      fathom = inputs.self.packages.${final.system}.fathom;
+    };
+
+    devShells = builtins.mapAttrs (system: pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          flutter344
+          dart
+        ];
+      };
+    }) inputs.nixpkgs.legacyPackages;
+  };
+}
